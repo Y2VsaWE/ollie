e545fc192ec5431de5b7daf51a68c5fd79a9d6c6
Célia Dominé <celia.domine@meteoblue.com>
2025-12-04 19:34:08 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ollie',
+  webDir: 'www'
+};
+
+export default config;
